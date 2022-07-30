0f7dab7ad613c80c1241d0bf45c6dd1e00dc9b93
BOBOTIK1 <volodko.97@bk.ru>
2022-07-30 17:21:38 +0300

    add js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n){
+    n = Math.abs(n)
+    nStr = String(n)
+    nArr = nStr.split('').reverse().join('')
+    return nArr
 }
+   
+
